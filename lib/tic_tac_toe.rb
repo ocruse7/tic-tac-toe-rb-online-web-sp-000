4d2cab7569bcfa01268dad422b9c0a3d26459c60
Owen Cruse <ocruse7@gmail.com>
2020-11-23 00:04:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,23 +9,11 @@ WIN_COMBINATIONS = [
   [6,4,2]
 ]
 
-def play(board)
-  while !over?(board)
-    turn(board)
-  end
-
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cats Game!"
-  end
-end
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
+  puts "--------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
+  puts "--------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
@@ -35,36 +23,32 @@ end
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
+    position(board, combo[0]) == position(board, combo[1]) &&
+    position(board, combo[1]) == position(board, combo[2]) &&
     position_taken?(board, combo[0])
   end
 end
 
-def full?(board)
-  board.all?{|token| token == "X" || token == "O"}
-end
-
 def draw?(board)
-  !won?(board) && full?(board)
-end
-
-def over?(board)
-  won?(board) || draw?(board)
+  !won?(board) && board.all?{|token| token == "X" || token == "O"}
 end
 
 def turn(board)
+  display_board(board)
   puts "Please enter 1-9:"
   input = gets.strip
   if !valid_move?(board, input)
     turn(board)
   end
-  move(board, input, current_player(board))
-  display_board(board)
+  move(board, input)
+end
+
+def position(board, location)
+  board[location.to_i]
 end
 
 def position_taken?(board, location)
-  !(board[location].nil? || board[location] == " ")
+  !(position(board, location).nil? || position(board, location) == "")
 end
 
 def current_player(board)
@@ -75,12 +59,26 @@ def turn_count(board)
   board.count{|token| token == "X" || token == "O"}
 end
 
-def move(board, location, player = "X")
-  board[location.to_i-1] = player
+def move(board, location)
+  if valid_move?(board, location)
+    board[location.to_i-1] = current_player(board)
+  end
 end
 
 def winner(board)
   if winning_combo = won?(board)
-    board[winning_combo.first]
+    position(board, winning_combo.first)
   end
+end
+
+board = Array.new(9)
+puts "Welcome to Tic Tac Toe"
+while !(won?(board) || draw?(board))
+  turn(board)
+end
+
+if won?(board)
+  puts "Congratulations! #{winner(board)}"
+elsif draw?(board)
+  puts "Cats Game!"
 end
